138b167c300ce66ae7995d99326dd1104fdb0dc9
kevin <kevin.castaneda@autozone.com>
2022-11-26 19:14:32 -0600

    updated auto config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://miwelomali.github.io',
-    base: '/Vallas-Gradas-CUU'
+    base: '/gradas-vallas-CUU'
 });
