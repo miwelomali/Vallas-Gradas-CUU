e10757c921224af1671ac73a72d20051f67a6ea2
kevin <kevin.castaneda@autozone.com>
2022-11-26 20:10:10 -0600

    Adding missing info for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://miwelomali.github.io',
-    base: '/Vallas-Gradas-CUU/'
+  site: 'https://miwelomali.github.io',
+  base: '/Vallas-Gradas-CUU',
+  output: "server",
+  adapter: netlify()
 });
